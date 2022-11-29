d5ff932956c5c46a000b3c6118354822f66b58eb
Rita Tagarao <39532457+Tirao121@users.noreply.github.com>
2022-11-29 14:56:46 -0700

    Update KillerRobotsMain.ino

--- KillerRobotsMain/KillerRobotsMain.ino
+++ KillerRobotsMain/KillerRobotsMain.ino
@@ -1,9 +1,50 @@
+/*
+ * Filename:
+ * Purpose:
+ * 
+ * Contributors:
+ * Date:
+ */
+
+//Libraries
+
+
+
+//Pins
+
+
+//Objects
+
+
+//Variables
+int mode = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Declare variables
+
+  //Switch between different modes
+  switch(mode) {
+    case 1:   //Standby
+      //scan and detect
+      break;
+    case 2:   //Chase or Attack
+      //PID
+      //Alert function
+      break;
+    case 3:   //Only Alert
+      //Alert only, arrived at obstacle or cannot reach
+      break;
+    case 4:   //Withdraw
+      //After 5s or so, withdraw from target
+      break;
+    default:  //Idle (all operations suspended)
+      //All operations off and safe
+    break;
+  }
 
 }
