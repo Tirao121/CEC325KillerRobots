0173e610fd411bbbc06c8a45db72686027d75d2b
LSetzkorn <thepercussionscout@gmail.com>
2022-12-12 14:56:25 -0700

    Update BLE-CentralPeripheralCombined.ino

--- Examples/BLE-CentralPeripheralCombined/BLE-CentralPeripheralCombined.ino
+++ Examples/BLE-CentralPeripheralCombined/BLE-CentralPeripheralCombined.ino
@@ -31,7 +31,12 @@ BLEByteCharacteristic modeCharacteristic(BLE_UUID_MODE, BLERead | BLEWrite | BLE
 
 // define which device this is
 #define setPeripheral 0   //Can change: 1 is peripheral, 0 is central
-char* robotName = "KROS"; //Don't change
+if (setPeripheral == 0) {
+  char* robotName = "KROSP"; //Don't change
+}
+else {
+  char* robotName = "KROSC"; //Don't change
+}
 
 // variables for bluetooth
 byte oldMode = 1;
@@ -201,7 +206,7 @@ void loopCentral () {
       Serial.print(peripheral.advertisedServiceUuid());
       Serial.println();
 
-      if (peripheral.localName() != robotName) {
+      if (peripheral.localName() != "KROSP") {
         Serial.print("Wrong local name: ");
         Serial.println(peripheral.localName());
         return;
